d9514f6fee1633c37e6db3102a049ce03730dd54
ej-shafran <ejshafran@hilma.tech>
2024-04-08 00:05:08 +0300

    test(rules): add `no-duplicate-route-params` unit tests

---
+++ test/no-duplicate-route-params.test.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const rule = require("../lib/rules/no-duplicate-route-params");
+const { RuleTester } = require("@typescript-eslint/rule-tester");
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-duplicate-route-params", rule, {
+  valid: [
+    {
+      name: "should ignore unrelated cases",
+      code: [
+        "@Controller('user')",
+        "export class UserController {",
+        "  nonRouteMethod() {}",
+        "",
+        "  @Get()",
+        "  get() {}",
+        "}",
+      ].join("\n"),
+    },
+    {
+      name: "should ignore non-duplicate param decorators",
+      code: [
+        "@Controller('user')",
+        "export class UserController {",
+        "  @Get('children/:id')",
+        "  getChildrenById() {}",
+        "}",
+      ].join("\n"),
+    },
+  ],
+
+  invalid: [
+    {
+      name: "should flag duplicate route parameters",
+      code: [
+        "@Controller('user')",
+        "export class UserController {",
+        "  @Get(':id/children/:id')",
+        "  getChildrenById() {}",
+        "}",
+      ].join("\n"),
+      errors: [
+        {
+          messageId: "duplicateRouteParam",
+          data: {
+            param: "id",
+          },
+        },
+      ],
+    },
+  ],
+});
