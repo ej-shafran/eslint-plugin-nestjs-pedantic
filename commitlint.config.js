787fe3cdca1ee8dec3c20a8110b7759af074860c
ej-shafran <ejshafran@hilma.tech>
2025-01-01 00:35:54 +0200

    ci(commitlint): ignore dependabot bumps

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,8 @@
 /** @type {import("@commitlint/types").UserConfig} */
 export default {
   extends: ["@commitlint/config-conventional"],
-  ignores: [(commit) => !!commit.match(/^\d\.\d\.\d$/)],
+  ignores: [
+    (commit) => !!commit.match(/^\d\.\d\.\d$/),
+    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+  ],
 };
